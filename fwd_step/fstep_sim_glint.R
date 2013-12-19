c4a5c8714d47b021eb87f7fc95ab0d32d65dc0ae
Joshua Loftus <joftius@stanford.edu>
2013-12-18 17:49:41 -0800

    Don't know how to normalize coeffs, that may be why glinternet isn't working

---
+++ fwd_step/fstep_sim_glint.R
@@ -0,0 +1,113 @@
+
+source('../generate_data.R')
+source('fstep_minimal.R')
+
+
+##########################
+### Modify these lines ###
+fname = "p20"           ##
+ns = c(100) #, 200, 400)   ##
+ps = 20 # floor(ns/2)        ##
+nsim = 20               ##
+klist = 1:5
+#########################
+#fname = "p2n"          ##
+#ns = c(100) #, 200)    ##
+#ps = ceiling(ns*2)     ##
+#nsim = 75              ##
+#kplus = 10             ##
+##########################
+corr = 0
+##########################
+
+lkl = length(klist)
+
+for (i in 1:length(ns)) {
+  mu.mat = matrix(0, nrow=lkl, ncol = nsim)
+  pow.mat = mu.mat
+  partialpow.mat = mu.mat
+  
+  n = ns[i]
+  p = ps[i]
+  mult = sqrt(2*log(p+p*(p-1)/2))
+  upper.coeff = 2.9
+  lower.coeff = 2.1
+  upper = upper.coeff*mult
+  lower = lower.coeff*mult
+  klist = 3*klist
+  groups = 1:p
+
+  
+  for (j in 1:lkl) {
+    k = klist[j]
+    print(k)
+    
+    for (iter in 1:nsim) {
+      # Generate data
+      print(iter)
+      data = generate_glinternet(gaussian_design(n, groups, corr = corr), groups)
+      X = data$X
+
+      all.groups = data$all.groups
+      main.groups = data$main.groups
+      int.groups = data$int.groups
+      beta = beta_glinternet(all.groups=all.groups, num.nonzero=k, upper=upper, lower=lower)
+      m = k/3
+      ptl.set = unique(all.groups)
+      r = length(ptl.set)
+      true.groups = c(ptl.set[1:m], ptl.set[(r-2*m+1):r])
+
+      
+      Y = X %*% beta + rnorm(n)
+      
+      # Calculate coherence
+ #     mu.mat[j, iter] = coherence(X)
+      
+      # Fit forward stepwise
+      added.groups = fstep_fit(X=X, Y=Y, groups=all.groups, max.steps=k)
+      true.added = intersect(added.groups, true.groups)
+      false.added = setdiff(added.groups, true.groups)
+      pow.mat[j, iter] = length(true.added) / k
+
+      # Calculate more generous version of power
+      # Includes cases where a main effect is added but the interaction is not
+      # and cases where interactions are added that should not have been
+      partialpow.mat[j, iter] = power_glinternet(k, main.groups, all.groups, int.groups, added.groups)
+
+    }
+  }
+  
+  power.MCavg = rowMeans(pow.mat)
+  ppower.MCavg = rowMeans(partialpow.mat)
+
+  print(rbind(klist, power.MCavg, ppower.MCavg))
+
+  # Save plot
+  plot.main = paste0("n = ", n, ", p = ", p, ", signal strength ", lower.coeff, "/", upper.coeff)
+  filename = paste0('../figs/fwdstep/gauss_glint_', fname, "_n", n, "_lower", lower.coeff, "_upper", upper.coeff)
+  if (corr != 0) {
+    filename = paste0(filename, "_corr", corr)
+    plot.main = paste0(plot.main, ", corr = ", corr)
+  }
+  filename = paste0(filename, ".pdf")
+  
+  pdf(filename)
+  plot(klist, power.MCavg, type = "l", main = plot.main, xlab = "Sparsity", ylab = "Average power", ylim = c(-0.1, 1.1), lwd = 2)
+  points(klist, ppower.MCavg, type="l", lty = "dotted", lwd = 2, col = "green")
+  abline(h = c(.9, .7, .5, .3, .1), lty = 2, col = "gray")
+  abline(h = c(1, .8, .6, .4, .2, 0), lty = 3, col = "gray")
+  
+  for (j in 1:lkl) {
+    xjitter = 0.05*rnorm(nsim)
+    yjitter = 0.01*rnorm(nsim)
+#    alphas = 2*mu.mat[j, ]
+#    alphas = sapply(alphas, function(a) min(a, 1))
+    points(rep(klist[j], nsim) + xjitter, pow.mat[j, ] + yjitter, pch = ".", col = rgb(red=0, green=0, blue=1), cex = 2)
+  }
+  dev.off()
+}
+
+
+
+
+
